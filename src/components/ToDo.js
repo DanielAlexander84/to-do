0f518a95ed994f6cbd92b00bdc0a744428af50ff
DanielAlexander84 <daberg84@gmail.com>
2022-09-12 20:27:05 +0200

    finalize todo-app

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,7 +1,18 @@
 import React from "react"
 
-function ToDo({ todo }) {
-  return <div className="todo">{todo.text}</div>
+function ToDo({ todo, index, completeTodo }) {
+  return (
+    <div>
+      {todo.text}
+      {todo.text === "keine To-dos vorhanden" ? (
+        <></>
+      ) : (
+        <div>
+          <button onClick={() => completeTodo(index)}>Erledigt</button>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default ToDo
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,20 +1,50 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import ToDo from "./ToDo"
+import ToDoForm from "./ToDoForm"
 
 function ToDoList() {
-  const [todos, setTodos] = useState([
-    { text: "Todo 1" },
-    { text: "Todo 2" },
-    { text: "Todo 3" },
-  ])
+  const placeholderTodoText = "keine To-dos vorhanden"
+
+  const [todos, setTodos] = useState([{ text: placeholderTodoText }])
+
+  useEffect(() => {
+    setTodos(JSON.parse(window.sessionStorage.getItem("todos")))
+  }, [])
+
+  useEffect(() => {
+    window.sessionStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }]
+    setTodos(newTodos)
+  }
+
+  const completeTodo = (indexToRemove) => {
+    const newTodos = todos.filter((item, index) => index !== indexToRemove)
+    if (todos.length <= 1) {
+      setTodos([{ text: placeholderTodoText }])
+    } else {
+      setTodos(newTodos)
+    }
+  }
+
+  if (todos.length > 1 && todos[0].text === placeholderTodoText) {
+    setTodos(todos.filter((item, index) => index !== 0))
+  }
 
   return (
-    <div className="app">
-      <div className="todo-list">
-        {todos.map((todo, index) => (
-          <ToDo key={index} index={index} todo={todo} />
-        ))}
-      </div>
+    <div>
+      <h1>Todos</h1>
+      {todos.map((todo, index) => (
+        <ToDo
+          key={index}
+          index={index}
+          todo={todo}
+          completeTodo={completeTodo}
+        />
+      ))}
+      <ToDoForm addTodo={addTodo} />
     </div>
   )
 }
